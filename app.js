272d464938de7f6a469cafb337c753dee5d6b42f
vmbreath <vmbreath@gmail.com>
2020-12-24 18:57:31 +0300

    added port

--- app.js
+++ app.js
@@ -17,4 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.listen(process.env.PORT || 3000)
+
 module.exports = app;
