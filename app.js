cbf2a84e1e6db84c447ad3938f0574d67963d13c
vmbreath <vmbreath@gmail.com>
2020-12-24 19:13:31 +0300

    removed wrong config

--- app.js
+++ app.js
@@ -12,16 +12,14 @@ var app = express();
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
-app.get("", (request, response) => {
-    response.send("Hello world")
-})
+// app.get("", (request, response) => {
+//     response.send("Hello world")
+// })
 
-let port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`app started on port ${port}`))
 
 module.exports = app;
