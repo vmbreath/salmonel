3246ab9e92d02ce0628bb1572d04b08f74679c28
vmbreath <65503957+vmbreath@users.noreply.github.com>
2021-02-16 16:55:26 +0300

    Update app.js
    Try to delete cors options

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const corsOptions = {
 }
 
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
