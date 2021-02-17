380a18a2c1bbe6405582f0cc3e5ca6a5262b4672
vmbreath <65503957+vmbreath@users.noreply.github.com>
2021-02-17 19:47:29 +0300

    added createTableAuthorization.js

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+import {createTableAuthorization} from "./createTableAuthorization";
 
 
-const whitelist = ['http://localhost:3000', 'https://salmonel-heroku.herokuapp.com/', 'http://localhost:3000/#/admin']
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
+// const whitelist = ['http://localhost:3000', 'https://salmonel-heroku.herokuapp.com/', 'http://localhost:3000/#/admin']
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
 
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -76,38 +77,40 @@ pool.query(`CREATE TABLE if not exists salmonel (
     });
 });
 
-// language=SQL format=false
-pool.query(`CREATE TABLE if not exists user_account (
-                id serial primary key,
-                name varchar ( 256 ),
-                login varchar ( 256 ),
-                password char( 64 ),
-                salt char ( 64 )
-    );`, (err, res) => {
-    if (err) throw err;
+createTableAuthorization(fs,pool);
 
-    pool.query('SELECT count(*) as count FROM user_account;', (err, res) => {
-        if (err) throw err;
-
-        console.log('SELECT count(*) as count FROM user_account: ', res.rows)
-
-        if (res.rows[0].count == '0') {
-            const sql = `INSERT INTO user_account (name, login, password, salt)
-                         VALUES ($1, $2, $3, $4);`;
-            const salt = crypto.createHash('sha256').update(new Date().getTime() + '').digest('hex');
-            pool.query(sql, [
-                'Admin',
-                'admin',
-                crypto.createHash('sha256').update('qwerty').update(salt).digest('hex'),
-                salt,
-            ], (err, res) => {
-                if (err) {
-                    return console.log(err.message);
-                }
-            })
-        }
-    })
-})
+// // language=SQL format=false
+// pool.query(`CREATE TABLE if not exists user_account (
+//                 id serial primary key,
+//                 name varchar ( 256 ),
+//                 login varchar ( 256 ),
+//                 password char( 64 ),
+//                 salt char ( 64 )
+//     );`, (err, res) => {
+//     if (err) throw err;
+//
+//     pool.query('SELECT count(*) as count FROM user_account;', (err, res) => {
+//         if (err) throw err;
+//
+//         console.log('SELECT count(*) as count FROM user_account: ', res.rows)
+//
+//         if (res.rows[0].count == '0') {
+//             const sql = `INSERT INTO user_account (name, login, password, salt)
+//                          VALUES ($1, $2, $3, $4);`;
+//             const salt = crypto.createHash('sha256').update(new Date().getTime() + '').digest('hex');
+//             pool.query(sql, [
+//                 'Admin',
+//                 'admin',
+//                 crypto.createHash('sha256').update('qwerty').update(salt).digest('hex'),
+//                 salt,
+//             ], (err, res) => {
+//                 if (err) {
+//                     return console.log(err.message);
+//                 }
+//             })
+//         }
+//     })
+// })
 
 
 // app.use(logger('dev'));
---
+++ createTableAuthorization.js
@@ -0,0 +1,36 @@
+function createTableAuthorization(fs,pool) {
+    // language=SQL format=false
+    pool.query(`CREATE TABLE if not exists user_account (
+                id serial primary key,
+                name varchar ( 256 ),
+                login varchar ( 256 ),
+                password char( 64 ),
+                salt char ( 64 )
+    );`, (err, res) => {
+        if (err) throw err;
+
+        pool.query('SELECT count(*) as count FROM user_account;', (err, res) => {
+            if (err) throw err;
+
+            console.log('SELECT count(*) as count FROM user_account: ', res.rows)
+
+            if (res.rows[0].count == '0') {
+                const sql = `INSERT INTO user_account (name, login, password, salt)
+                         VALUES ($1, $2, $3, $4);`;
+                const salt = crypto.createHash('sha256').update(new Date().getTime() + '').digest('hex');
+                pool.query(sql, [
+                    'Admin',
+                    'admin',
+                    crypto.createHash('sha256').update('qwerty').update(salt).digest('hex'),
+                    salt,
+                ], (err, res) => {
+                    if (err) {
+                        return console.log(err.message);
+                    }
+                })
+            }
+        })
+    })
+}
+
+export { createTableAuthorization };
